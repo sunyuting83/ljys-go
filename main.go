a901ce84b9cc917006bd3d0afd318bfb9d1e40c0
sun <sleepsun123@163.com>
2020-06-06 01:37:06 +0800

    add ignore and add flag

--- main.go
+++ main.go
@@ -1,18 +1,24 @@
 package main
 
 import (
+	"flag"
 	orm "newapp/database"
 	leveldb "newapp/leveldb"
 	"newapp/router"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	var port string
+	flag.StringVar(&port, "p", "3000", "端口号，默认为3000")
+	flag.Parse()
+
 	gin.SetMode(gin.ReleaseMode)
 	defer orm.Eloquent.Close()
 
 	defer leveldb.Leveldb.Close()
 	router := router.InitRouter()
-	router.Run(":8456")
+	router.Run(strings.Join([]string{":", port}, ""))
 }
